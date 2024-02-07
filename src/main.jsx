469fd1bdf944c1453ee665092256ac0d83242a89
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-06 17:55:15 -0800

    Create router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+    {
+        path: "/message-client",
+        element: <App />,
+        errorElement: <ErrorPage />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
