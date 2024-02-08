fa586133bee6dcf4d07d2c2b87937bc48ec1a08c
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-08 15:06:19 -0800

    Add logic to handle getting and creating messages

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,11 +1,112 @@
-import Message from "./Message";
+import { useState, useEffect } from "react";
 
-function Messages() {
-    // TODO: Fetch all messages for the channel prop and pass to message component
+import MessageCard from "./MessageCard";
+
+function Messages({ selectedChannel }) {
+    const [messages, setMessages] = useState("");
+    const [numMessages, setNumMessages] = useState(0);
+    const [newMessage, setNewMessage] = useState("");
+
+    const [formError, setFormError] = useState("");
+    const [error, setError] = useState("");
+
+    //Get all Channel Messages
+    useEffect(() => {
+        async function getMessages() {
+            try {
+                const response = await fetch(
+                    `https://message-api.fly.dev/api/channels/${selectedChannel._id}/messages`,
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            authorization: `Bearer ${localStorage.getItem(
+                                "token"
+                            )}`,
+                        },
+                    }
+                );
+
+                if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`
+                    );
+                }
+
+                const data = await response.json();
+                console.log(data);
+
+                setMessages(data);
+                setError("");
+            } catch (err) {
+                setError(err.message);
+                setUser("");
+            }
+        }
+        getMessages();
+    }, [numMessages]);
+
+    async function createNewMessage(e) {
+        e.preventDefault();
+
+        setFormError("");
+        setError("");
+
+        const formData = JSON.stringify({
+            content: newMessage,
+        });
+
+        // Make request to create new Message
+        try {
+            const response = await fetch(
+                `https://message-api.fly.dev/api/channels/${selectedChannel._id}/messages`,
+                {
+                    method: "post",
+                    body: formData,
+                    headers: {
+                        "Content-Type": "application/json",
+                        authorization: `Bearer ${localStorage.getItem(
+                            "token"
+                        )}`,
+                    },
+                }
+            );
+
+            console.log(response);
+            const result = await response.json();
+            console.log(result);
+
+            // Handle any errors
+            if (response.status == 400) {
+                setFormError(result.errors);
+            } else if (!response.ok) {
+                throw new Error(
+                    `This is an HTTP error: The status is ${response.status}`
+                );
+            } else {
+                setNewMessage("");
+                let val = numMessages + 1;
+                setNumMessages(val);
+            }
+        } catch (err) {
+            setError(err.message);
+        }
+    }
 
     return (
         <div className="messages">
-            <div>Messages Container</div>
+            <MessageCard />
+            <form className="newMessageForm">
+                <div className="formElement">
+                    <input
+                        type="text"
+                        name="message"
+                        id="message"
+                        placeholder="New Message"
+                        value={newMessage}
+                        onChange={(e) => setNewMessage(e.target.value)}
+                    />
+                </div>
+            </form>
         </div>
     );
 }
