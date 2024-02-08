7daf0d3b36c2578ce656fbfdfc3d150d6f225c6f
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-08 15:38:14 -0800

    Create channel card

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,9 @@
+function ChannelCard() {
+    return (
+        <div className="channelCard">
+            <div>This is a channel card</div>
+        </div>
+    );
+}
+
+export default ChannelCard;
