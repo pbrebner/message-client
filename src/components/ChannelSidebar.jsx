5a202e5287d0e03dacee55b18b88aa43e1d52649
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-08 15:39:43 -0800

    Add logic to get and create channels

--- src/components/ChannelSidebar.jsx
+++ src/components/ChannelSidebar.jsx
@@ -1,9 +1,101 @@
-function ChannelSidebar() {
-    // TODO: Display all the channels
+import { useState, useEffect } from "react";
+
+import ChannelCard from "./ChannelCard";
+
+function ChannelSidebar({ setSelectedChannel }) {
+    const [channels, setChannels] = useState("");
+    const [numChannels, setNumChannels] = useState(0);
+
+    const [formError, setFormError] = useState("");
+    const [error, setError] = useState("");
+
+    //Get all users channels
+    useEffect(() => {
+        async function getChannels() {
+            try {
+                const response = await fetch(
+                    "https://message-api.fly.dev/api/channels",
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            authorization: `Bearer ${localStorage.getItem(
+                                "token"
+                            )}`,
+                        },
+                    }
+                );
+
+                if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`
+                    );
+                }
+
+                const data = await response.json();
+                console.log(data);
+
+                setChannels(data);
+                setError("");
+            } catch (err) {
+                setError(err.message);
+                setUser("");
+            }
+        }
+        getChannels();
+    }, [numChannels]);
+
+    async function createNewChannel(e) {
+        e.preventDefault();
+
+        setFormError("");
+        setError("");
+
+        const formData = JSON.stringify({
+            title: "",
+            users: [],
+        });
+
+        // Make request to create new Message
+        try {
+            const response = await fetch(
+                "https://message-api.fly.dev/api/channels",
+                {
+                    method: "post",
+                    body: formData,
+                    headers: {
+                        "Content-Type": "application/json",
+                        authorization: `Bearer ${localStorage.getItem(
+                            "token"
+                        )}`,
+                    },
+                }
+            );
+
+            console.log(response);
+            const result = await response.json();
+            console.log(result);
+
+            // Handle any errors
+            if (response.status == 400) {
+                setFormError(result.errors);
+            } else if (!response.ok) {
+                throw new Error(
+                    `This is an HTTP error: The status is ${response.status}`
+                );
+            } else {
+                //setNewMessage("");
+                let val = numChannels + 1;
+                setNumChannels(val);
+            }
+        } catch (err) {
+            setError(err.message);
+        }
+    }
 
     return (
         <div className="channelSidebar">
-            <div>Channel Sidebar</div>
+            <button onClick={createNewChannel}>Create New Channel</button>
+            <ChannelCard />
         </div>
     );
 }
