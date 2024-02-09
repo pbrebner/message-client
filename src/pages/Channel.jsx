d825ff25749281cb1677b442d94ba8696baf0e96
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-08 16:46:15 -0800

    Clean up code

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -7,53 +7,11 @@ import Messages from "../components/Messages";
 import UserSidebar from "../components/UserSidebar";
 
 function Channel() {
-    const [user, setUser] = useState("");
-
     const [error, setError] = useState("");
 
     const navigate = useNavigate();
 
     //TODO: Confirm that the user is logged In, otherwise redirect to login
-
-    // Fetch the User
-    useEffect(() => {
-        async function getUser() {
-            try {
-                const response = await fetch(
-                    `https://message-api.fly.dev/api/users/${localStorage.getItem(
-                        "userId"
-                    )}`,
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                            authorization: `Bearer ${localStorage.getItem(
-                                "token"
-                            )}`,
-                        },
-                    }
-                );
-
-                if (response.status == "403") {
-                    // Invalid Token
-                    navigate("/message-client/login");
-                } else if (!response.ok) {
-                    throw new Error(
-                        `This is an HTTP error: The status is ${response.status}`
-                    );
-                }
-
-                const data = await response.json();
-                console.log(data);
-
-                setUser(data.user);
-                setError("");
-            } catch (err) {
-                setError(err.message);
-                setUser("");
-            }
-        }
-        getUser();
-    }, []);
 
     return (
         <div className="channelsPage">
