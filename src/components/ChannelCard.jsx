7dc68991e6e6284b69d2c5e654e2da8da0b0bf62
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-09 18:52:08 -0800

    Update layout and styling

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,40 @@
-function ChannelCard() {
+import { useNavigate } from "react-router-dom";
+
+import "./styles/ChannelCard.css";
+
+function ChannelCard({ channel }) {
+    const navigate = useNavigate();
+
+    let channelUsers = "";
+    channel.users.forEach((user, index) => {
+        if (index != 0) {
+            channelUsers = channelUsers + ", " + user.name;
+        } else {
+            channelUsers = channelUsers + user.name;
+        }
+    });
+
+    function deleteChannel() {
+        console.log("Deleting Channel");
+    }
+
+    function handleClick() {
+        navigate(`/message-client/channels/${channel._id}`);
+    }
+
     return (
-        <div className="channelCard">
-            <div>This is a channel card</div>
+        <div className="channelCard" onClick={handleClick}>
+            <div className="channelCardInnerContainer">
+                <div className="channelCardImage">Img</div>
+                {channel.title ? (
+                    <div className="channelCardText">{channel.title}</div>
+                ) : (
+                    <div className="channelCardText">{channelUsers}</div>
+                )}
+            </div>
+            <button className="deleteChannelCard" onClick={deleteChannel}>
+                &#x2715;
+            </button>
         </div>
     );
 }
