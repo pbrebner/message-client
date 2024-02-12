c8bcc2fd6636e6324abe32dd309968717d816798
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-12 13:38:24 -0800

    Update styling

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./styles/ChannelCard.css";
 
 function ChannelCard({ channel }) {
+    const [hover, setHover] = useState(false);
+
     let channelUsers = "";
     channel.users.forEach((user, index) => {
         if (index != 0) {
@@ -17,7 +20,11 @@ function ChannelCard({ channel }) {
     }
 
     return (
-        <div className="channelCard">
+        <div
+            className="channelCard"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+        >
             <Link
                 to={`../channels/${channel._id}`}
                 className="channelCardInnerContainer"
@@ -29,7 +36,10 @@ function ChannelCard({ channel }) {
                     <div className="channelCardText">{channelUsers}</div>
                 )}
             </Link>
-            <button className="deleteChannelCard" onClick={deleteChannel}>
+            <button
+                className={`deleteChannelCardBtn ${hover ? "display" : ""}`}
+                onClick={deleteChannel}
+            >
                 &#x2715;
             </button>
         </div>
