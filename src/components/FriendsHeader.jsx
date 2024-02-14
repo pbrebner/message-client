2b7fd8f8d271702447448872d9dce9d46985688b
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-13 19:03:01 -0800

    Add selection logic to header

--- src/components/FriendsHeader.jsx
+++ src/components/FriendsHeader.jsx
@@ -1,15 +1,50 @@
 import "./styles/FriendsHeader.css";
 
-function FriendsHeader() {
+function FriendsHeader({
+    showNone,
+    showAll,
+    setShowAll,
+    showPending,
+    setShowPending,
+    showAdd,
+    setShowAdd,
+}) {
     return (
         <div className="friendsHeader">
             <div className="friendsHeaderTitle">Friends</div>
             <div className="friendsHeaderDivider"></div>
             <div className="friendsHeaderBtns">
-                <button className="friendsHeaderBtn">All</button>
-                <button className="friendsHeaderBtn">Online</button>
-                <button className="friendsHeaderBtn">Pending</button>
-                <button className="friendsHeaderBtn">Add Friend</button>
+                <button
+                    className={`friendsHeaderBtn ${showAll ? "selected" : ""}`}
+                    onClick={() => {
+                        showNone();
+                        setShowAll(true);
+                    }}
+                >
+                    All
+                </button>
+                <button
+                    className={`friendsHeaderBtn ${
+                        showPending ? "selected" : ""
+                    }`}
+                    onClick={() => {
+                        showNone();
+                        setShowPending(true);
+                    }}
+                >
+                    Pending
+                </button>
+                <button
+                    className={`friendsHeaderBtn addFriendBtn ${
+                        showAdd ? "selected" : ""
+                    }`}
+                    onClick={() => {
+                        showNone();
+                        setShowAdd(true);
+                    }}
+                >
+                    Add Friend
+                </button>
             </div>
         </div>
     );
