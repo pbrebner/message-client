90200e3a01e97c69f319eb3cdfd8455e540f7bf9
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-14 15:48:52 -0800

    Build userSidebar layout and style

--- src/components/UserSidebar.jsx
+++ src/components/UserSidebar.jsx
@@ -1,11 +1,27 @@
+import { formatDate } from "../utils/dates";
 import "./styles/UserSidebar.css";
 
-function UserSidebar() {
-    //TODO: Fetch data for channel users and display data
-
+function UserSidebar({ otherUsers }) {
     return (
         <div className="userSidebar">
-            <div>User Sidebar</div>
+            {otherUsers &&
+                otherUsers.map((user) => (
+                    <div className="userCard">
+                        <div className="userCardMain">
+                            <div className="userCardImg">
+                                <img src={user.avatar} />
+                            </div>
+                            <div className="userCardName">{user.name}</div>
+                        </div>
+                        <div className="userCardDivider"></div>
+                        <div className="userCardSupp">
+                            <div>{user.bio}</div>
+                            <div>
+                                Member since: {formatDate(user.timeStamp)}
+                            </div>
+                        </div>
+                    </div>
+                ))}
         </div>
     );
 }
