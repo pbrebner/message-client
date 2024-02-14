63fdf2bcfcdeef888e4f55603e7116e17a8f1e3f
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-13 16:09:34 -0800

    Add btns, menu, and likes to card

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
 import { formatDate } from "../utils/dates.js";
+import like from "../assets/icons/like.png";
 import "./styles/MessageCard.css";
 
 function MessageCard({ message, numMessages, setNumMessages }) {
+    const [hover, setHover] = useState(false);
+    const [modalOpen, setModalOpen] = useState(false);
     const [messageLikes, setMessageLikes] = useState(0);
+
+    // TODO: Set it up so only the user can delete his own messages
 
     async function likeMessage() {
         console.log("Liked message");
@@ -14,9 +19,23 @@ function MessageCard({ message, numMessages, setNumMessages }) {
         console.log("Deleting message");
     }
 
+    function toggleModal() {
+        modalOpen ? setModalOpen(false) : setModalOpen(true);
+    }
+
     return (
-        <div className="messageCard">
-            <div className="messageImage">User Image</div>
+        <div
+            className={`messageCard ${hover || modalOpen ? "hover" : ""}`}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+        >
+            <div className="messageImageContainer">
+                <img
+                    src={message.user.avatar}
+                    alt="User"
+                    className="messageImage"
+                />
+            </div>
             <div className="message">
                 <div className="messageHeader">
                     <div className="messageUser">{message.user.name}</div>
@@ -25,7 +44,38 @@ function MessageCard({ message, numMessages, setNumMessages }) {
                     </div>
                 </div>
                 <p className="messageContents">{message.content}</p>
+                {message.likes > 0 && (
+                    <div className="messageLikes">
+                        <img src={like} />
+                        <div>{message.likes}</div>
+                    </div>
+                )}
             </div>
+            <div
+                className={`messageCardBtns ${
+                    hover || modalOpen ? "display" : ""
+                }`}
+            >
+                <button className="reactBtn" onClick={likeMessage} title="Like">
+                    <img src={like} />
+                </button>
+                <button className="moreBtn" onClick={toggleModal} title="More">
+                    &#8230;{" "}
+                </button>
+            </div>
+            <div className={`messageCardModal ${modalOpen ? "display" : ""}`}>
+                <button className="messageCardModalBtn">Reply</button>
+                <button
+                    className="messageCardModalBtn deleteMessageBtn"
+                    onClick={deleteMessage}
+                >
+                    Delete Message
+                </button>
+            </div>
+            <div
+                className={`overlay ${modalOpen ? "display" : ""}`}
+                onClick={toggleModal}
+            ></div>
         </div>
     );
 }
