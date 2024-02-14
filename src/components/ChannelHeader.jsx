86d149d6ca8441b854f3b43520aa7ea6bedba514
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-14 15:00:54 -0800

    Update channel header

--- src/components/ChannelHeader.jsx
+++ src/components/ChannelHeader.jsx
@@ -1,9 +1,22 @@
 import "./styles/ChannelHeader.css";
 
-function ChannelHeader({ channel }) {
+function ChannelHeader({ otherUsers }) {
     return (
         <div className="channelHeader">
-            <p>This is the channel Header</p>
+            <div className="channelHeaderUsers">
+                {otherUsers &&
+                    otherUsers.map((user) => (
+                        <div key={user._id} className="channelHeaderUser">
+                            <div className="channelHeaderUserImg">
+                                <img src={user.avatar} />
+                            </div>
+                            <div> {user.name}</div>
+                        </div>
+                    ))}
+            </div>
+            <div className="channelHeaderActions">
+                <button className="channelHeaderBtn">Add user to DM</button>
+            </div>
         </div>
     );
 }
