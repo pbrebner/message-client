fdb35fff6da0da8fce996b5f00712b0be3b930ee
Patrick Brebner <patrick.brebner@gmail.com>
2024-02-23 14:16:00 -0800

    Create layout for account and function to delete user

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,7 +1,199 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate, useOutletContext } from "react-router-dom";
+
+import Button from "../components/Button";
+import "./styles/Account.css";
+
 function Account() {
+    const [user, setUser] = useState("");
+
+    const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+
+    const [showLoader, setShowLoader] = useState(false);
+
+    const [error, setError] = useState("");
+
+    const [loggedIn, setLoggedIn] = useOutletContext();
+    const navigate = useNavigate();
+
+    // Fetch the User
+    useEffect(() => {
+        async function getUser() {
+            try {
+                const response = await fetch(
+                    `https://message-api.fly.dev/api/users/${localStorage.getItem(
+                        "userId"
+                    )}`,
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            authorization: `Bearer ${localStorage.getItem(
+                                "token"
+                            )}`,
+                        },
+                    }
+                );
+
+                if (response.status == "403") {
+                    // Invalid Token
+                    navigate("/message-client/login");
+                } else if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`
+                    );
+                }
+
+                const data = await response.json();
+                console.log(data);
+
+                setUser(data.user);
+                setError("");
+            } catch (err) {
+                setError(err.message);
+                setUser("");
+            }
+        }
+        getUser();
+    }, []);
+
+    function handleLogOut() {
+        setLoggedIn(false);
+        localStorage.clear();
+        navigate("/message-client/login");
+    }
+
+    async function deleteUser() {
+        setShowLoader(true);
+        setError("");
+
+        // Make request to delete User
+        try {
+            const response = await fetch(
+                `https://message-api.fly.dev/api/users/${user._id}`,
+                {
+                    method: "delete",
+                    headers: {
+                        "Content-Type": "application/json",
+                        authorization: `Bearer ${localStorage.getItem(
+                            "token"
+                        )}`,
+                    },
+                }
+            );
+
+            const result = await response.json();
+            console.log(result);
+
+            setShowLoader(false);
+            setDeleteModalOpen(false);
+
+            if (!response.ok) {
+                throw new Error(
+                    `This is an HTTP error: The status is ${response.status}`
+                );
+            } else {
+                handleLogOut();
+            }
+        } catch (err) {
+            setError(err.message);
+        }
+    }
+
+    function closeModals() {
+        setDeleteModalOpen(false);
+    }
+
     return (
         <div className="account">
-            <h1>Account Page</h1>
+            <div className="accountMain">
+                <div className="accountHeader">
+                    <h2>My Account</h2>
+                    <Link to="/message-client/channels" className="accountLink">
+                        Back to Channels
+                    </Link>
+                </div>
+                <div className="accountInfo">
+                    <div className="accountInfoHeader">
+                        <div>
+                            <div className="accountImg">
+                                <img src={user.avatar} />
+                            </div>
+                            <div className="accountName">{user.name}</div>
+                        </div>
+                        <button className="accountBtn">
+                            Edit User Profile
+                        </button>
+                    </div>
+                    <div className="accountDivider"></div>
+                    <div className="accountInfoMain">
+                        <div className="accountInfoSection">
+                            <div>
+                                <p>Name</p>
+                                <div>{user.name}</div>
+                            </div>
+                            <button className="editBtn">Edit</button>
+                        </div>
+                        <div className="accountInfoSection">
+                            <div>
+                                <p>Bio</p>
+                                <div>{user.bio}</div>
+                            </div>
+                            <button className="editBtn">Edit</button>
+                        </div>
+                        <div className="accountInfoSection">
+                            <div>
+                                <p>Email</p>
+                                <div>{user.email}</div>
+                            </div>
+                            <button className="editBtn">Edit</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="accountDivider"></div>
+                <div className="accountActions">
+                    <h2>Log Out</h2>
+                    <button
+                        className="accountBtn logOutBtn"
+                        onClick={handleLogOut}
+                    >
+                        Log Out
+                    </button>
+                </div>
+                <div className="accountDivider"></div>
+                <div className="accountActions">
+                    <h2>Account Removal</h2>
+                    <p>
+                        Deleting the account is permanent and can't be undone.
+                    </p>
+                    <button
+                        className="deleteAccountBtn"
+                        onClick={() => setDeleteModalOpen(true)}
+                    >
+                        Delete Account
+                    </button>
+                </div>
+            </div>
+            <div className={`accountModal ${deleteModalOpen ? "display" : ""}`}>
+                <h2>Confirm Delete?</h2>
+                <p>Deleting the account is permanent and can't be undone.</p>
+                <div className="accountDivider"></div>
+                <div className="accountModalBtns">
+                    <button className="accountBtn" onClick={closeModals}>
+                        Cancel
+                    </button>
+                    <Button
+                        styleRef="deleteAccountBtn"
+                        onClick={deleteUser}
+                        text="Confirm Delete"
+                        loading={showLoader}
+                        disabled={showLoader}
+                    />
+                </div>
+            </div>
+            <div
+                className={`overlay ${deleteModalOpen ? "display" : ""}`}
+                onClick={closeModals}
+            ></div>
         </div>
     );
 }
