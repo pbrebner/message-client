4cbd113e213181b73f4a8e9b2de41355e43b532d
Patrick Brebner <patrick.brebner@gmail.com>
2024-04-15 16:06:45 -0700

    Add socket

--- src/pages/Channels.jsx
+++ src/pages/Channels.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate, Outlet, useOutletContext } from "react-router-dom";
+import { socket } from "../utils/socket";
 
 import ChannelSidebar from "../components/ChannelSidebar";
 import PageLoader from "../components/PageLoader";
@@ -16,69 +17,74 @@ function Channels() {
     const [pageLoading, setPageLoading] = useState(true);
     const [showSidebar, setShowSidebar] = useState(false);
 
-    const [loggedIn, setLoggedIn, setError, socket] = useOutletContext();
+    const [loggedIn, setLoggedIn, setError] = useOutletContext();
     const navigate = useNavigate();
 
     // Fetch the User
     useEffect(() => {
-        getUser();
+        // Function fetches the user data
+        async function getUser() {
+            try {
+                const response = await fetch(
+                    `https://message-api.fly.dev/api/users/${localStorage.getItem(
+                        "userId"
+                    )}`,
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            authorization: `Bearer ${localStorage.getItem(
+                                "token"
+                            )}`,
+                        },
+                    }
+                );
 
-        setTimeout(() => {
-            setPageLoading(false);
-        }, "2000");
+                const data = await response.json();
+                //console.log(data);
+
+                // handle fetch response
+                if (response.status == "401") {
+                    // Invalid Token
+                    socket.emit("offline", {
+                        userId: `${localStorage.getItem("userId")}`,
+                    });
+
+                    navigate("/message-client/login", {
+                        state: { message: "Your Session Timed Out." },
+                    });
+                } else if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`
+                    );
+                } else {
+                    setUser(data.user);
+                    setNumChannels(data.user.channels.length);
+                    setNumFriends(data.user.friends.length);
+                    setError("");
+                }
+            } catch (err) {
+                setError(err.message);
+                setUser("");
+            }
+        }
 
         // Emits that user is online and triggers online status change on backend
-        // This causes getUser to run multiple times
-        socket.emit("online", { userId: `${localStorage.getItem("userId")}` });
+        socket.emit("online", {
+            userId: `${localStorage.getItem("userId")}`,
+        });
+
+        // On socket
+        socket.on("receiveOnline", getUser);
+
+        // Set timeout for page loading
+        setTimeout(() => {
+            setPageLoading(false);
+        }, "2500");
+
+        return () => {
+            socket.off("receiveOnline", getUser);
+        };
     }, []);
-
-    // On socket
-    socket.on("receiveOnline", () => {
-        console.log("running socket");
-        getUser();
-    });
-
-    // Function fetches the user data
-    async function getUser() {
-        try {
-            const response = await fetch(
-                `https://message-api.fly.dev/api/users/${localStorage.getItem(
-                    "userId"
-                )}`,
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                        authorization: `Bearer ${localStorage.getItem(
-                            "token"
-                        )}`,
-                    },
-                }
-            );
-
-            const data = await response.json();
-            console.log(data);
-
-            // handle fetch response
-            if (response.status == "401") {
-                // Invalid Token
-                navigate("/message-client/login", {
-                    state: { message: "Your Session Timed Out." },
-                });
-            } else if (!response.ok) {
-                throw new Error(
-                    `This is an HTTP error: The status is ${response.status}`
-                );
-            } else {
-                setUser(data.user);
-                setNumChannels(data.user.channels.length);
-                setNumFriends(data.user.friends.length);
-                setError("");
-            }
-        } catch (err) {
-            setError(err.message);
-            setUser("");
-        }
-    }
 
     function closeSidebar() {
         setShowSidebar(false);
@@ -118,7 +124,6 @@ function Channels() {
                         numFriends,
                         setNumFriends,
                         setError,
-                        socket,
                     ]}
                 />
                 <div
