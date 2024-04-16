8cf74556f3977d56db493c900e880169a4dda5a7
Patrick Brebner <patrick.brebner@gmail.com>
2024-04-15 17:01:19 -0700

    Update url and add error handling

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,15 @@
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect("https://message-api.fly.dev");
+
+socket.on("connect_error", (error) => {
+    if (socket.active) {
+        // temporary failure, the socket will automatically try to reconnect
+    } else {
+        // the connection was denied by the server
+        // in that case, `socket.connect()` must be manually called in order to reconnect
+        console.log(error.message);
+    }
+});
 
 export { socket };
